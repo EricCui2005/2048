7480b3ce81f69114999cec85d567cef816b60673
ericcui <ericcui2005@gmail.com>
2024-11-28 01:54:46 -0500

    Finished game

--- 2048/game.py
+++ 2048/game.py
@@ -164,6 +164,7 @@ class Game(tk.Frame):
         self.stack()
         self.add_new_tile()
         self.update_GUI()
+        self.game_over()
     
     
     def right(self, event):
@@ -174,6 +175,7 @@ class Game(tk.Frame):
         self.reverse()
         self.add_new_tile()
         self.update_GUI()
+        self.game_over()
     
     
     def up(self, event):
@@ -184,6 +186,7 @@ class Game(tk.Frame):
         self.transpose()
         self.add_new_tile()
         self.update_GUI()
+        self.game_over()
     
     
     def down(self, event):
@@ -196,6 +199,7 @@ class Game(tk.Frame):
         self.transpose()
         self.add_new_tile()
         self.update_GUI()
+        self.game_over()
     
     # Check if any moves are possible
     
@@ -228,4 +232,21 @@ class Game(tk.Frame):
                 fg=c.GAME_OVER_FONT,
                 font=c.GAME_OVER_FONT
             ).pack()
-        elif not any(0 in row for row in self.matrix)
+        elif not any(0 in row for row in self.matrix) and not self.horizontal_move_exists() and not self.vertical_move_exists():
+            game_over_frame = tk.Frame(self.main_grid, borderwidth=2)
+            game_over_frame.place(relx=0.5, rely=0.5, anchor="center")
+            tk.Label(
+                game_over_frame,
+                text="Game over!",
+                bg=c.LOSER_BG,
+                fg=c.GAME_OVER_FONT_COLOR,
+                font=c.GAME_OVER_FONT
+            ).pack()
+
+
+def main():
+    Game()
+
+
+if __name__ == "__main__":
+    main()
