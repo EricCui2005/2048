d09d36ec4041673b08eb3d73afd9e598ef84e7f2
AliAhmad810 <hiimaliahmad@gmail.com>
2024-12-07 18:08:08 -0800

    data logging

--- src/experiment.py
+++ src/experiment.py
@@ -1,3 +1,4 @@
+import csv
 from game.game_logic import Game
 from players.mcts_player import MCTSPlayer
 from players.random_player import Random
@@ -8,40 +9,58 @@ def trials(player, num_trials):
     game = Game()
     res = {'Wins': 0, 'Losses': 0}
     
-    for i in range(num_trials):
-        
-        game.game_init()
-        
-        while True:
+    with open('data/run1.csv', mode='w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+
+        for i in range(num_trials):
             
-            move = player.move(game)
+            game.game_init()
+
             
-            # UWU - eric
-            match move:
-                case 'up':
-                    game.up()
-                case 'left':
-                    game.left()
-                case 'down':
-                    game.down()
-                case 'right':
-                    game.right()
+            data = []
+            
+            while True:
+                current_state = game.matrix
+                current_score = game.score
+
+                move = player.move(game)
                 
-            if game.game_over() == 1:
-                print(f"Trial {i + 1}: Win")
-                res['Wins'] += 1
-                break
-            if game.game_over() == -1:
-                print(f"Trial {i + 1}: Loss")
-                res['Losses'] += 1
-                break
+                # UWU - eric
+                match move:
+                    case 'up':
+                        game.up()
+                    case 'left':
+                        game.left()
+                    case 'down':
+                        game.down()
+                    case 'right':
+                        game.right()
+
+                next_state = game.matrix
+                next_score = game.score
+
+                reward = next_score - current_score
+
+                data.append((i, current_state, current_score, move, reward, next_state, next_score))
+                    
+                if game.game_over() == 1:
+                    print(f"Trial {i + 1}: Win")
+                    res['Wins'] += 1
+                    for row in data:
+                        processed_row = [str(element) if isinstance(element, list) else element for element in row]
+                        writer.writerow(processed_row)
+                    break
+                if game.game_over() == -1:
+                    print(f"Trial {i + 1}: Loss")
+                    res['Losses'] += 1
+                    break
     
     return res
 
 
 def main():
-    print(trials(player=MCTSPlayer(simulations=40, rollouts=100, c=100, discount=0.9), num_trials=100))
-        
+    print(trials(player=MCTSPlayer(simulations=100, rollouts=10, c=100, discount=0.9), num_trials=20))
+
 
 if __name__ == "__main__":
     main()
